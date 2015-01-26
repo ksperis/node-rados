9fd0033090734973ce2d430a45ed03fb82bd84eb
Laurent Barbe <laurent@ksperis.com>
2015-01-26 10:09:00 +0100

    test code from wcc21320

--- rados.h
+++ rados.h
@@ -89,6 +89,7 @@ class Ioctx : public node::ObjectWrap {
   static NAN_METHOD(aio_write_full);
   static NAN_METHOD(aio_flush);
   static NAN_METHOD(aio_flush_async);
+  static NAN_METHOD(aio_objects_list);
   static v8::Persistent<v8::FunctionTemplate> constructor;
 };
 
